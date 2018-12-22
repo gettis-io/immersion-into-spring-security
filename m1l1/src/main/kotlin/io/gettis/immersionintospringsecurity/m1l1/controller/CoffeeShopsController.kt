3b63c3aa0944fb8ad4848cb46f201fbc142a59c2
Ivan Osipov <i.osipov.develop@gmail.com>
2018-12-22 23:29:15 +0300

    added m1l1

---
+++ m1l1/src/main/kotlin/io/gettis/immersionintospringsecurity/m1l1/controller/CoffeeShopsController.kt
@@ -0,0 +1,19 @@
+package io.gettis.immersionintospringsecurity.m1l1.controller
+
+import io.gettis.immersionintospringsecurity.m1l1.model.CoffeeShop
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/coffeeShops")
+class CoffeeShopsController {
+
+    @GetMapping("")
+    fun getAll(): List<CoffeeShop> {
+        return listOf(
+                CoffeeShop("Best Coffee")
+        )
+    }
+
+}
---
+++ m1l1/src/main/kotlin/io/gettis/immersionintospringsecurity/m1l1/model/CoffeeShop.kt
@@ -0,0 +1,5 @@
+package io.gettis.immersionintospringsecurity.m1l1.model
+
+class CoffeeShop(
+        var name: String
+)
